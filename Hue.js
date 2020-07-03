c06cc4132e671eced66a9082e470572a11cf97b1
ManuN <Manuel.nuebel@gmx.de>
2020-07-03 09:20:18 +0200

    Update Hue.js

--- Hue.js
+++ Hue.js
@@ -8,7 +8,7 @@ var mymap = L.map('mapid').setView([16.35,107.6], 10);
 var OpenStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
+}).addTo(mymap);
 
 
 var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
